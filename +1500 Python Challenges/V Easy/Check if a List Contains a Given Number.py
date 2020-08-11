369294476283803fa32a5fb9eb0b833760c62a70
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-11 12:35:02 +0200

    creat v easy challenges

---
+++ +1500 Python Challenges/V Easy/Check if a List Contains a Given Number.py
@@ -0,0 +1,61 @@
+"""
+
+Check if a List Contains a Given Number
+Write a function to check if a list contains a particular number.
+
+Examples
+check([1, 2, 3, 4, 5], 3) ➞ True
+
+check([1, 1, 2, 1, 1], 3) ➞ False
+
+check([5, 5, 5, 6], 5) ➞ True
+
+check([], 5) ➞ False
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
