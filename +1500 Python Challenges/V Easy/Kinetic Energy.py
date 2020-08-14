3f56570ab8ac4d48c341f42cb57f49d0b8c9395d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-14 15:09:26 +0200

    create edabit challenges

---
+++ +1500 Python Challenges/V Easy/Kinetic Energy.py
@@ -0,0 +1,64 @@
+"""
+
+Kinetic Energy
+Kinetic energy can be calculated with the following formula:
+
+KE = 1/2mv²
+
+m is mass in kg
+v is velocity in m/s
+KE is kinetic energy in J
+Return the Kinetic Energy in Joules, given the mass and velocity. For the purposes of this challenge, round answers to the nearest integer.
+
+Examples
+calc_kinetic_energy(60, 3) ➞ 270
+
+calc_kinetic_energy(45, 10) ➞ 2250
+
+calc_kinetic_energy(63.5, 7.35) ➞ 1715
+Notes
+Expect only positive numbers for inputs.
+
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
