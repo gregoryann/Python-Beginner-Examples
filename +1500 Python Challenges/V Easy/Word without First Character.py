e03bd164677d541f6b289a9c2fcdaf933e9a9254
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-14 14:44:36 +0200

    create initial edabit challenges

---
+++ +1500 Python Challenges/V Easy/Word without First Character.py
@@ -0,0 +1,58 @@
+"""
+
+
+
+Word without First Character
+Create a function that takes a word and returns the new word without including the first character.
+
+Examples
+new_word("apple") ➞ "pple"
+
+new_word("cherry") ➞ "herry"
+
+new_word("plum") ➞ "lum"
+Notes
+The input is always a valid word.
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
