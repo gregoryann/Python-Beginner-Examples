e5deeec5eaa56ca7554cf03eecad74c935c64780
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-14 14:29:33 +0200

    create initial edabit challenges

---
+++ +1500 Python Challenges/V Easy/Is the Last Character an N.py
@@ -0,0 +1,59 @@
+"""
+
+s the Last Character an N?
+Create a function that takes a string (a random name). If the last character of the name is an "n", return True, otherwise return False.
+
+Examples
+is_last_character_n("Aiden") ➞ True
+
+is_last_character_n("Piet") ➞ False
+
+is_last_character_n("Bert") ➞ False
+
+is_last_character_n("Dean") ➞ True
+Notes
+The function must return a boolean value ( i.e. True or False).
+
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
