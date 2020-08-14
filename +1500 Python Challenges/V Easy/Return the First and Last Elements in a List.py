f40b6794376d5361edb8000d11becf77a5372f31
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-14 15:02:16 +0200

    add edabit v easy challenges

---
+++ +1500 Python Challenges/V Easy/Return the First and Last Elements in a List.py
@@ -0,0 +1,59 @@
+"""
+
+Return the First and Last Elements in a List
+Create a function that takes a list of elements and return the first and last elements as a new list.
+
+Examples
+first_last([5, 10, 15, 20, 25]) ➞ [5, 25]
+
+first_last(["edabit", 13, None, False, True]) ➞ ["edabit", True]
+
+first_last([None, 4, "6", "hello", None]) ➞ [None, None]
+Notes
+Test input will always contain a minimum of two elements within the list.
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
