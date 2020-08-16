91b1ded97f88e70e91cd65cc9d253985de08802d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-16 12:54:40 +0200

    update challenges comments

--- +1500 Python Challenges/V Easy/Broken Bridge.py
+++ +1500 Python Challenges/V Easy/Broken Bridge.py
@@ -1,6 +1,16 @@
 """
 
+Broken Bridge
+Create a function which validates whether a bridge is safe to walk on (i.e. has no gaps in it to fall through).
 
+Examples
+is_safe_bridge("####") ➞ True
+
+is_safe_bridge("## ####") ➞ False
+
+is_safe_bridge("#") ➞ True
+Notes
+You can expect the bridge's ends connecting it to its surrounding.
 
 
 """
--- +1500 Python Challenges/V Easy/Nth Star Number.py
+++ +1500 Python Challenges/V Easy/Nth Star Number.py
@@ -1,6 +1,18 @@
 """
 
+Nth Star Number
+Create a function that takes a positive integer and returns the nth "star number".
 
+A star number is a centered figurate number a centered hexagram (six-pointed star), such as the one that Chinese checkers is played on.
+
+Examples
+star_number(2) ➞ 13
+
+star_number(3) ➞ 37
+
+star_number(5) ➞ 121
+Notes
+See Resources for more information.
 
 
 """
