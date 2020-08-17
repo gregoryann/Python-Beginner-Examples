7960cf241b1c9f00063f9d25c381cdd554112dc6
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-17 15:47:20 +0200

    create edabit challenges

---
+++ +1500 Python Challenges/V Easy/Convert Yen to USD.py
@@ -0,0 +1,59 @@
+"""
+
+Convert Yen to USD
+Create a function that can turn Yen (Japanese dollar) to USD (American dollar).
+
+Examples
+yen_to_usd(1) ➞ 0.01
+
+yen_to_usd(500) ➞ 4.65
+
+yen_to_usd(649) ➞ 6.04
+Notes
+Each Yen to USD conversion is Yen / 107.5
+
+
+
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
