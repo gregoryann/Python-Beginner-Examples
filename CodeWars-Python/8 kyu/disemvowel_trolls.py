a6a45ad16688b7661849e3c103f4820e59c4ed00
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-18 09:53:15 +0200

    Create disemvowel_trolls.py

---
+++ CodeWars-Python/8 kyu/disemvowel_trolls.py
@@ -0,0 +1,24 @@
+"""
+Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels
+from the troll's comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string
+with all vowels removed.
+For example, the string 'This website is for losers LOL!' would become
+'Ths wbst s fr lsrs LL!'.
+Note: For this kata 'y' isn't considered a vowel.
+"""
+
+# My Answer
+def disemvowel(string):
+    condition = 'aAeEiIoOuU'
+    filter = [character for character in string if character not in condition]
+    return ''.join(filter)
+
+# Best Answer
+def disemvowel(s):
+    return s.translate(None, "aeiouAEIOU")
+
+#Tests
+test.assert_equals(disemvowel("This website is for losers LOL!"),
+                              "Ths wbst s fr lsrs LL!")
