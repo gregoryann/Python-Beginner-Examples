26ba7a57856aebecad10215b0919ff53f9a9092e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-19 10:26:35 +0200

    add solution to kyu8 TestForVariableType

--- CodeWars-Python/8 kyu/TestForVariableType.py
+++ CodeWars-Python/8 kyu/TestForVariableType.py
@@ -6,3 +6,36 @@ Please leave feedback for this kata. Cheers!
 Tags: FUNDAMENTALS
 Link: https://www.codewars.com/kata/who-ate-the-cookie/train/python
 """
+#############################################################
+#                        MY SOLUTIONS                       #
+#############################################################
+
+# turns out that you can actually use these basic type classes as dict keys:
+names = {str: "Zach", float: "Monica", int: "Monica"}
+
+def cookie(x):
+    return "Who ate the last cookie? It was {}!".format(names.get(type(x), "the dog"))
+
+
+
+#############################################################
+#             OTHER INTERESTING SOLUTIONS & REMARKS         #
+#############################################################
+
+
+
+
+
+#############################################################
+#                           TESTS                           #
+#############################################################
+
+import sys, os
+sys.path.append(os.path.abspath(
+                os.path.join(os.path.dirname(__file__), '..', "tests")))
+from Test import *
+
+test.assert_equals(cookie("Ryan"), "Who ate the last cookie? It was Zach!")
+test.assert_equals(cookie(2.3), "Who ate the last cookie? It was Monica!")
+test.assert_equals(cookie(26), "Who ate the last cookie? It was Monica!")
+test.assert_equals(cookie(True), "Who ate the last cookie? It was the dog!")
