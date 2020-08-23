94454050ecc6faafeecaae473ebb26e2dec62f26
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-23 13:58:57 +0200

    add solutions to edabit v easy challenges

---
+++ +1500 Python Challenges/V Easy/Sort Numbers in Ascending Order.py
@@ -0,0 +1,58 @@
+"""
+Sort Numbers in Ascending Order
+Create a function that takes a list of numbers and returns a new list, sorted in ascending order (smallest to biggest).
+
+Sort numbers list in ascending order.
+If the function's argument is None or an empty list; return an empty list.
+Return a new array of sorted numbers.
+Examples
+sort_nums_ascending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+
+sort_nums_ascending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
+
+sort_nums_ascending([]) ➞ []
+Notes
+Test input can be positive or negative.
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+
+
+
+
+
+
+"""
+Solution 2
+"""
+
+
+
+
+
+"""
+Solution 3
+"""
+
+
+
+
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
