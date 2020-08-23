08576ed335e10adc7f39cd7384a8cad7eadb3347
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-23 13:41:29 +0200

    add solutions to edabit v easy challenges

---
+++ +1500 Python Challenges/V Easy/Format I Template String.py
@@ -0,0 +1,64 @@
+"""
+Format I: Template String
+For each challenge of this series you do not need to submit a function. Instead, you need to submit a template string that can formatted in order to get a certain outcome.
+
+Write a template string according to the following example:
+
+Example
+a = "John"
+b = "Joe"
+c = "Jack"
+template = "yourtemplatestringhere"
+template.format(a, b, c) ➞ "Their names were: John, Joe and Jack."
+Tips
+A template string is a string that uses curly braces {} as a placeholder that can then be formatted:
+
+"hello, my name is {}".format("John") ➞ "hello, my name is John."
+Notes
+Submit a string, not a function.
+Do not change the name of the variable template.
+You can find all the exercises in this series over here.
+
+"""
+
+
+
+
+
+
+"""
+Solution 1
+"""
+
+template = "Their names were: {}, {} and {}."
+
+
+"""
+Solution 2
+"""
+
+template = "Their names were: {}, {} and {}."
+a = "John"
+b = "Joe"
+c = "Jack"
+template.format(a, b, c)
+
+"""
+Solution 3
+"""
+
+a = "John"
+b = "Joe"
+c = "Jack"
+template = "Their names were: {}, {} and {}."
+template.format(a, b, c)
+
+"""
+Solution 4
+"""
+
+
+
+
+
+
