ad744be397db94cd5d6a3d19b8d2aca16d9a9fb2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-28 13:44:05 +0200

    add solutions to CodeWars Kyu8

---
+++ CodeWars-Python/8 kyu/UnexpectedParsing.py
@@ -0,0 +1,14 @@
+'''
+Description:
+
+This code is buggy. Can you fix it?
+'''
+
+
+#############################################################
+#                        MY SOLUTIONS                       #
+#############################################################
+
+
+def get_status(is_busy): return {'status': ("busy" if is_busy else "available")}
+
