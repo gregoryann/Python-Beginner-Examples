6cb84370905aeeb5e82e4ffe9702471259e4c214
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-28 13:42:20 +0200

    add solutions to CodeWars Kyu8

---
+++ CodeWars-Python/8 kyu/RegexpBasicsIsItADigit.py
@@ -0,0 +1,20 @@
+
+
+'''
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a
+digit (0-9), false otherwise.
+'''
+
+
+#############################################################
+#                        MY SOLUTIONS                       #
+#############################################################
+
+
+import re
+
+def is_digit(n):
+    return bool(re.match(r'^\d\Z$', n))
+
+
+print(is_digit('14'))
