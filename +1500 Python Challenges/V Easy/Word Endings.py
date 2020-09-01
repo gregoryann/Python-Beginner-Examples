92db0beeaca9bfcaa84c170898f957ea8c102714
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-01 13:55:38 +0200

    added solution to edabit/ v easy challenge 'Word Endings'

---
+++ +1500 Python Challenges/V Easy/Word Endings.py
@@ -0,0 +1,64 @@
+"""
+Word Endings
+Create a function that adds a string ending to each member in a list.
+
+Examples
+add_ending(["clever", "meek", "hurried", "nice"], "ly")
+➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+
+add_ending(["new", "pander", "scoop"], "er")
+➞ ["newer", "panderer", "scooper"]
+
+add_ending(["bend", "sharpen", "mean"], "ing")
+➞ ["bending", "sharpening", "meaning"]
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+"""
+
+
+"""
+Solution 1
+"""
+
+def add_ending(lst, ending):
+	return [ word + ending for word in lst]
+
+"""
+Solution 2
+"""
+
+add_ending=lambda l,e:[i+e for i in l]
+
+"""
+Solution 3
+"""
+
+def add_ending(lst, ending):
+    new_lst = []
+    for word in lst:
+           new_lst.append(word + ending)
+    return new_lst
+
+"""
+Solution 4
+"""
+
+def add_ending(lst, ending):
+	for i in lst:
+		j = i+ending
+		lst.append(j)
+	return j
+
+"""
+Solution 5
+"""
+
+def add_ending(lst, ending):
+	for x in range(len(lst)):
+		lst[x] = lst[x] + ending
+	return lst
+
+
