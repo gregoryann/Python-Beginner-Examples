65deffaeb25f43ae1c8b228c75f869f4f6307958
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-04 18:18:38 +0200

    added solution to CodeWars Kyu8  sum_mixed_array

---
+++ CodeWars-Python/8 kyu/SumMixedArray.py
@@ -0,0 +1,15 @@
+"""Sum Mixed Array
+https://www.codewars.com/kata/sum-mixed-array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+"""
+
+
+#############################################################
+#                        MY SOLUTIONS                       #
+#############################################################
+
+def sum_mix(arr):
+    return sum(map(int, arr))
