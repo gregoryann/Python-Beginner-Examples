0d35027b1ec5070fa2c698168b90d39b79601234
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-05 12:38:13 +0200

    update

--- +1500 Python Challenges/V Easy/Join Two Portions of a Path.py
+++ +1500 Python Challenges/V Easy/Join Two Portions of a Path.py
@@ -1,12 +1,18 @@
 """
 
+Join Two Portions of a Path
+Write a function that receives two portions of a path and joins them. The portions will be joined with the "/" separator. There could be only one separator and if it is not present it should be added.
 
+Examples
+join_path("portion1", "portion2") ➞ "portion1/portion2"
+join_path("portion1/", "portion2") ➞ "portion1/portion2"
+join_path("portion1", "/portion2") ➞ "portion1/portion2"
+join_path("portion1/", "/portion2") ➞ "portion1/portion2"
 
+Notes
+Try not to solve this challenge using only if-else conditions.
 
 """
-
-
-
 
 
 
@@ -14,33 +20,15 @@
 Solution 1
 """
 
-
-
-
-
-
+def join_path(portion1, portion2):
+	return '/'.join((portion1.rstrip('/'), portion2.lstrip('/')))
 
 """
 Solution 2
 """
 
-
-
-
-
-"""
-Solution 3
-"""
-
-
-
-
-
-"""
-Solution 4
-"""
-
-
+def join_path(portion1, portion2):
+  return "".join([p for p in portion1 if p != "/"] + ["/"] + [p for p in portion2 if p != "/"])
 
 
 
