7fa84cd27f2e9f7112b9cd722004eef71b3b96c2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-13 14:01:39 +0200

    added solution to edabit/ easy 'Recursion: Factorials'

---
+++ +1500 Python Challenges/Easy/Recursion Factorials.py
@@ -0,0 +1,71 @@
+"""
+Recursion: Factorials
+
+Write a function that calculates the factorial of a number recursively.
+
+Examples
+factorial(5) ➞ 120
+
+factorial(3) ➞ 6
+
+factorial(1) ➞ 1
+
+factorial(0) ➞ 1
+
+"""
+
+
+
+"""
+Solution 1
+"""
+def factorial(n):
+    return n * factorial(n - 1) if n > 1 else 1
+
+"""
+Solution 2
+"""
+
+def factorial(n):
+	if n==0: return 1
+	return n*factorial(n-1)
+"""
+Solution 3
+"""
+
+def factorial(n):
+	if n <= 1:
+		return 1
+	return n * factorial(n - 1)
+
+"""
+Solution 4
+"""
+
+factorial=lambda n:1if n<2else n*factorial(n-1)
+
+"""
+Solution 5
+"""
+
+def factorial(n):
+	import math
+	return math.factorial(n)
+
+"""
+Solution 6
+"""
+
+import numpy as np
+def factorial(n):
+	return np.product([x for x in range (1,n+1)])
+
+"""
+Solution 7
+"""
+
+def factorial(n):
+	if n == 1 or n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
