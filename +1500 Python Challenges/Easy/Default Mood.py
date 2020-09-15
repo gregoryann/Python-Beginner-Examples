108636b31f5ab6438ca03bee3c26d55deef14a85
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-15 13:05:38 +0200

    added solution to edabit/ easy 'Default Mood'

---
+++ +1500 Python Challenges/Easy/Default Mood.py
@@ -0,0 +1,42 @@
+"""
+Default Mood
+
+Create a function that takes in a current mood and return a sentence in the following format: "Today, I am feeling {mood}". However, if no argument is passed, return "Today, I am feeling neutral".
+
+Examples
+mood_today("happy") ➞ "Today, I am feeling happy"
+
+mood_today("sad") ➞ "Today, I am feeling sad"
+
+mood_today() ➞ "Today, I am feeling neutral"
+
+Notes
+Check the Resources tab for some helpful information.
+"""
+
+
+"""
+Solution 1
+"""
+
+def mood_today(mood = 'neutral'):
+	return 'Today, I am feeling '+ mood
+
+"""
+Solution 2
+"""
+
+def mood_today(mood='neutral'):
+	return 'Today, I am feeling {}'.format(mood)
+
+"""
+Solution 3
+"""
+
+def mood_today(*mood):
+	return "Today, I am feeling {}".format(mood[0] if mood else 'neutral')
+
+
+
+
+
