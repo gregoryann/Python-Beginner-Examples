98e2cb34dfb41f32faaeb221847a3fc44dc668be
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-18 18:48:46 +0200

    added solutions to edabit/ easy 'Filter Strings from Array'

---
+++ +1500 Python Challenges/Easy/Filter Strings from Array.py
@@ -0,0 +1,55 @@
+"""
+
+Filter Strings from Array
+Create a function that takes a list of strings and integers, and filters out the list so that it returns a list of integers only.
+
+Examples
+filter_list([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
+
+filter_list(["A", 0, "Edabit", 1729, "Python", "1729"]) ➞ [0, 1729]
+
+filter_list(["Nothing", "here"]) ➞ []
+
+Notes
+Don't overthink this one.
+"""
+
+
+"""
+Solution 1
+"""
+
+def filter_list(l):
+  return [i for i in l if type(i)==int]
+
+"""
+Solution 2
+"""
+
+def filter_list(l):
+	new = []
+	for el in l:
+		if type(el) == type(1):
+			new.append(el)
+	return new
+
+"""
+Solution 3
+"""
+
+filter_list=lambda l:[e for e in l if 0*e==0]
+
+"""
+Solution 4
+"""
+
+def filter_list(l):
+	new = []
+	for x in l:
+		if str(x).lstrip("-+").isdigit():
+			new.append(x)
+	return new
+
+
+
+
