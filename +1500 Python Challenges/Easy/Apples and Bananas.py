66ae0e5a047d8b27e323cca2316dc9c65000ab06
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-22 18:13:29 +0200

    added solutions to edabit/ easy 'Apples and Bananas'

---
+++ +1500 Python Challenges/Easy/Apples and Bananas.py
@@ -0,0 +1,61 @@
+"""
+Apples and Bananas
+
+Write a function, that replaces all vowels in a string with a specified vowel.
+
+Examples
+vow_replace("apples and bananas", "u") ➞ "upplus und bununus"
+
+vow_replace("cheese casserole", "o") ➞ "chooso cossorolo"
+
+vow_replace("stuffed jalapeno poppers", "e") ➞ "steffed jelepene peppers"
+
+Notes
+All words will be lowercase. Y is not considered a vowel.
+"""
+
+
+
+"""
+Solution 1
+"""
+
+def vow_replace(txt, vowel):
+	return "".join(vowel if i in 'aeiou' else i for i in txt)
+
+"""
+Solution 2
+"""
+
+import re
+
+def vow_replace(word, vowel):
+	return re.sub('[aeiou]', vowel, word)
+
+"""
+Solution 3
+"""
+
+def vow_replace(word, vowel):
+	word = list(word)
+	for i in range(len(word)):
+		if word[i] in ['a','e','i','o','u']:
+			word[i] = vowel
+	return "".join(word)
+
+"""
+Solution 4
+"""
+
+def vow_replace(word, vowel):
+	sto=""
+	for x in word:
+		if x in "aeiou":
+			sto+=vowel
+		else:
+			sto+=x
+	return sto
+
+
+
+
