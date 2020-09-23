0b49208944a65cef661a03d75f3bcb88cfe41dd5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-23 12:08:08 +0200

    added solutions to edabit/ easy 'Recursion: Sum'

---
+++ +1500 Python Challenges/Easy/Recursion Sum.py
@@ -0,0 +1,49 @@
+"""
+Recursion: Sum
+
+Write a function that finds the sum of the first n natural numbers. Make your function recursive.
+
+Examples
+sum_numbers(5) ➞ 15
+# 1 + 2 + 3 + 4 + 5 = 15
+
+sum_numbers(1) ➞ 1
+
+sum_numbers(12) ➞ 78
+
+Notes
+Assume the input number is always positive.
+Check the Resources tab for info on recursion.
+"""
+
+
+
+"""
+Solution 1
+"""
+
+def sum_numbers(n):
+	return n + sum_numbers(n-1) if n else 0
+
+"""
+Solution 2
+"""
+
+sum_numbers=lambda n:0if n<1else n+sum_numbers(n-1)
+
+"""
+Solution 3
+"""
+
+def sum_numbers(n):
+	if n==0:
+		return 0
+	return n+sum_numbers(n-1)
+
+
+
+
+
+
+
+
