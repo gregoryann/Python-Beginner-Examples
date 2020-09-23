6ea25b2ce04976b92ef6730daed72f5794946aaa
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-23 12:17:39 +0200

    added solutions to edabit/ easy 'Count the Arguments'

---
+++ +1500 Python Challenges/Easy/Count the Arguments.py
@@ -0,0 +1,45 @@
+"""
+Count the Arguments
+
+Create a function that returns the number of arguments it was called with.
+
+Examples
+num_args() ➞ 0
+
+num_args("foo") ➞ 1
+
+num_args("foo", "bar") ➞ 2
+
+num_args(True, False) ➞ 2
+
+num_args({}) ➞ 1
+
+Notes
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+"""
+
+
+"""
+Solution 1
+"""
+
+def num_args(*args, **kwargs):
+	return len(args) + len(kwargs)
+
+"""
+Solution 2
+"""
+
+def num_args(*args):
+	n = 0
+	for i in args:
+		n += 1
+	return n
+
+
+
+
+
+
+
