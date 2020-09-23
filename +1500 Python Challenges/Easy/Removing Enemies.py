50848f7ec97bb0060f2066134becf2d1a3140712
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-23 12:16:07 +0200

    added solutions to edabit/ easy 'Removing Enemies'

---
+++ +1500 Python Challenges/Easy/Removing Enemies.py
@@ -0,0 +1,46 @@
+"""
+Removing Enemies
+Remove enemies from the list of people, even if the enemy shows up twice.
+
+Examples
+remove_enemies(["Fred"], []) ➞ ["Fred"]
+
+remove_enemies(["Adam", "Emmy", "Tanya", "Emmy"], ["Emmy"]) ➞ ["Adam", "Tanya"]
+
+remove_enemies(["John", "Emily", "Steve", "Sam"], ["Sam", "John"]) ➞ ["Emily", "Steve"]
+
+Notes
+All names to be removed will be in the enemies list; simply return the list, no fancy strings.
+"""
+
+
+"""
+Solution 1
+"""
+
+def remove_enemies(names, enemies):
+	return [i for i in names if i not in enemies]
+
+"""
+Solution 2
+"""
+
+def remove_enemies(names, enemies):
+	return sorted(list(set(names)-set(enemies)),key=names.index)
+
+"""
+Solution 3
+"""
+
+def remove_enemies(names, enemies):
+	for enemy in enemies:
+		for name in names:
+			if name == enemy:
+				names.remove(name)
+				
+	return names
+
+
+
+
+
