1877b355c1c4d9fbf193b147b7344e7b505e21e9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-24 15:06:11 +0200

    added solutions to edabit/ easy 'Double Letters'

---
+++ +1500 Python Challenges/Easy/Double Letters.py
@@ -0,0 +1,50 @@
+"""
+Double Letters
+Create a function that takes a word and returns True if the word has two consecutive identical letters.
+
+Examples
+double_letters("loop") ➞ True
+
+double_letters("yummy") ➞ True
+
+double_letters("orange") ➞ False
+
+double_letters("munchkin") ➞ False
+"""
+
+
+
+"""
+Solution 1
+"""
+
+def double_letters(word):
+	return any(i*2 in word for i in word)
+
+"""
+Solution 2
+"""
+
+def double_letters(word):
+	for i in range(len(word)-1):
+		if word[i] == word[i+1]:
+			return True
+	return False
+
+"""
+Solution 3
+"""
+
+def double_letters(word):
+  import re
+  return len((re.compile(r'(\w)\1')).findall(word)) >= 1
+
+"""
+Solution 4
+"""
+
+double_letters = lambda w: any(x*2 in w for x in w)
+
+
+
+
