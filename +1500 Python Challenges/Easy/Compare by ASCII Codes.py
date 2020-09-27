b9f9a982af777a7c38b9275dd14ecf243f24bf7c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-09-27 12:33:18 +0200

    added solutions to edabit/ easy 'Compare by ASCII Codes'

--- +1500 Python Challenges/Easy/Compare by ASCII Codes.py
+++ +1500 Python Challenges/Easy/Compare by ASCII Codes.py
@@ -37,12 +37,25 @@ def ascii_sort(lst):
 Solution 3
 """
 
+def ascii_sort(arr):
+        wrd1total = 0
+        wrd2total = 0
+        word1 = list(arr[0])
+        word2 = list(arr[1])
+        for ltr in word1:
+                wrd1total += ord(ltr)
+        for ltr in word2:
+                wrd2total += ord(ltr)
+        if wrd1total < wrd2total:
+                return arr[0]
+        else:
+                return arr[1]
 
 """
 Solution 4
 """
 
-
+ascii_sort=lambda l:min(l,key=lambda x:sum(map(ord,x)))
 
 
 
