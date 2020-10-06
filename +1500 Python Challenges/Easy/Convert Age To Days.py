c78dc7b021d15096042597739fc38296d03a95ae
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-10-06 19:23:09 +0200

    added solutions to edabit/ easy 'Convert Age To Days'

---
+++ +1500 Python Challenges/Easy/Convert Age To Days.py
@@ -0,0 +1,34 @@
+"""
+Convert Age To Days
+Create a function that takes the age and return the age in days.
+
+Examples
+calc_age(65) ➞ 23725
+
+calc_age(0) ➞ 0
+
+calc_age(20) ➞ 7300
+
+Notes
+Use 365 days as the length of a year for this challenge.
+Expect only positive integer inputs.
+"""
+
+
+
+
+
+################################################################
+"""
+Solution 1
+"""
+
+def calc_age(age):
+	return age*365
+
+
+
+
+
+
+
